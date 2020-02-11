09f08440c37f60b78a3e91f4e2cf46ee5ded8234
Nemoy-holmes <amitsingh20oct10@gmail.com>
2020-02-11 11:57:51 +0200

    commit-2-linking

---
+++ Game/pch.h
@@ -0,0 +1,14 @@
+#ifndef PCH_H
+#define PCH_H
+#include <stdio.h>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include<iostream>
+#include<cassert>
+// TODO: add headers that you want to pre-compile here
+
+#endif //PCH_H
